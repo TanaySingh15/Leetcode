4368baaa8a88c9cbe64dc48ec983647fee62fe14
Tanay Singh <59234540+TanaySingh15@users.noreply.github.com>
2024-08-18 14:47:08 +0530

    Time: 21 ms (34.60%) | Memory: 44.3 MB (25.95%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,42 @@
 class Solution {
+    int maxFreq=0;
     public int characterReplacement(String s, int k) {
-        int ans = 0;
-        int n = s.length();
-        for (char c = 'A'; c <= 'Z'; c++) {
-            int i = 0, j = 0, replaced = 0;
-            while (j < n) {
-                if (s.charAt(j) == c) {
-                    j++;
-                } else if (replaced < k) {
-                    j++;
-                    replaced++;
-                } else if (s.charAt(i) == c) {
-                    i++;
-                } else {
-                    i++;
-                    replaced--;
+        int start=0,end=0;
+        
+        int result=0;
+        Map<Character,Integer> map=new HashMap<>();
+        for(;end<s.length();end++){
+            char c=s.charAt(end);
+            map.put(c,map.getOrDefault(c,0)+1);
+            maxFreq=Math.max(maxFreq,map.get(c));
+            
+            if(end-start+1-maxFreq<=k){
+                result=Math.max(result,end-start+1);
+            }else{
+                while(end-start+1-maxFreq>k){
+                    removeFromMap(s.charAt(start++),map);
                 }
-                ans = Math.max(ans, j - i);
             }
         }
-        return ans;
+        
+        return result;
+    }
+    
+    public void removeFromMap(char c,Map<Character,Integer> map){
+        
+        if(map.get(c)==1){
+            map.remove(c);
+        }else{
+            map.put(c,map.get(c)-1);
+            if(map.get(c)+1==maxFreq){
+                setMaxFreq(map);
+            }
+        }
+    }
+    
+    public void setMaxFreq(Map<Character,Integer> map){
+        for(char k:map.keySet()){
+            this.maxFreq=Math.max(this.maxFreq,map.get(k));
+        }
     }
 }
