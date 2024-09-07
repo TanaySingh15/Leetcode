e0c4064dd65f0b74637ae1a60dfee1e6d5d15690
Tanay Singh <59234540+TanaySingh15@users.noreply.github.com>
2024-09-07 17:06:46 +0530

    Time: 23 ms (40.30%) | Memory: 56.5 MB (5.03%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        return topDown(text1,n,text2,m,dp);
+        return bottomUp(text1,n,text2,m);
     }
 
     public int recursive(String a,int n,String b,int m){
@@ -27,6 +27,19 @@ class Solution {
         else
             dp[n][m] = Math.max(topDown(a.substring(0,n-1),n-1,b,m,dp),topDown(a,n,b.substring(0,m-1),m-1,dp));
         
+        return dp[n][m];
+    }
+
+    public int bottomUp(String a,int n,String b,int m){
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1)==b.charAt(j-1))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
         return dp[n][m];
     }
 
