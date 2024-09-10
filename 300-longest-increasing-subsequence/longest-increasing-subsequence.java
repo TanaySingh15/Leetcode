f58cc71457cb36243ea42d295f9fac502cf4afe1
Tanay Singh <59234540+TanaySingh15@users.noreply.github.com>
2024-09-11 00:29:59 +0530

    Time: 69 ms (28.21%) | Memory: 76 MB (21.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,14 +1,14 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n+1;j++){
-                dp[i][j] = -1;
-            }
-        }
-        // return recursion(nums,n,0,-1);
-        return topDown(nums,n,0,-1,dp);
+        // int n = nums.length;
+        // int[][] dp = new int[n][n+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n+1;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return topDown(nums,n,0,-1,dp);
+        return bottomUp(nums);
     }
 
     int recursion(int[] nums,int n,int index,int prev_index){
@@ -29,5 +29,19 @@ class Solution {
             length = Math.max(length,topDown(nums,n,index+1,index,dp)+1);
         dp[index][prev_index+1] = length;
         return dp[index][prev_index+1];
+    }
+
+    int bottomUp(int[] nums){
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int index = n-1;index>=0;index--){
+            for(int prev_index = index-1;prev_index>=-1;prev_index--){
+                int length = dp[index+1][prev_index+1];
+                if(prev_index == -1 || nums[index]>nums[prev_index]) 
+                    length = Math.max(length,1 + dp[index+1][index+1]);
+                dp[index][prev_index+1] = length;
+            }
+        }
+        return dp[0][-1+1];
     }
 }
